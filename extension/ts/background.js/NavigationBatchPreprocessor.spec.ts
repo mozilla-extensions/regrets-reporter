21918223076693988f33179153edd71c4bba2fc1
Motin <motin@motin.eu>
2020-03-30 16:49:08 +0300

    Restored passing tests for navigation batch preprocessor

--- extension/ts/background.js/NavigationBatchPreprocessor.spec.ts
+++ extension/ts/background.js/NavigationBatchPreprocessor.spec.ts
@@ -31,38 +31,44 @@ describe("NavigationBatchPreprocessor", function() {
       firstVisitIsoDateTimeString,
     );
 
-    describe("Queue processing 5 seconds after the visit", function() {
-      const nowDateTime = addSeconds(firstVisitDateTime, 5);
-      it("should not yield any navigation batches to send", async function() {
-        navigationBatchPreprocessor.navigationBatchSendQueue = [];
-        await navigationBatchPreprocessor.processQueue(nowDateTime);
-        assert.equal(
-          navigationBatchPreprocessor.navigationBatchSendQueue.length,
-          0,
-        );
-      });
-    });
-
     describe("Queue processing 20 seconds after the visit", function() {
       const nowDateTime = addSeconds(firstVisitDateTime, 20);
-      it("should yield relevant navigation batches to send", async function() {
-        navigationBatchPreprocessor.navigationBatchSendQueue = [];
+      it("should have found one navigation batch", async function() {
+        navigationBatchPreprocessor.navigationBatchesByNavigationUuid = {};
         await navigationBatchPreprocessor.processQueue(nowDateTime);
         assert.equal(
           navigationBatchPreprocessor.studyPayloadEnvelopeProcessQueue.length,
-          0,
-        );
-        assert.equal(
-          navigationBatchPreprocessor.navigationBatchSendQueue.length,
           1,
         );
+        const navigationUuids = Object.keys(
+          navigationBatchPreprocessor.navigationBatchesByNavigationUuid,
+        );
+        assert.equal(navigationUuids.length, 1);
         const firstNavigationBatch: NavigationBatch =
-          navigationBatchPreprocessor.navigationBatchSendQueue[0];
+          navigationBatchPreprocessor.navigationBatchesByNavigationUuid[
+            navigationUuids[0]
+          ];
         assert.equal(firstNavigationBatch.childEnvelopes.length, 4);
         assert.equal(firstNavigationBatch.httpRequestCount, 2);
         assert.equal(firstNavigationBatch.httpResponseCount, 2);
         assert.equal(firstNavigationBatch.httpRedirectCount, 0);
         assert.equal(firstNavigationBatch.javascriptOperationCount, 0);
+      });
+    });
+
+    describe("Queue processing 10 hours after the visit", function() {
+      const nowDateTime = addSeconds(firstVisitDateTime, 10 * 60 * 60);
+      it("should have purged all navigation batches", async function() {
+        navigationBatchPreprocessor.navigationBatchesByNavigationUuid = {};
+        await navigationBatchPreprocessor.processQueue(nowDateTime);
+        assert.equal(
+          navigationBatchPreprocessor.studyPayloadEnvelopeProcessQueue.length,
+          0,
+        );
+        const navigationUuids = Object.keys(
+          navigationBatchPreprocessor.navigationBatchesByNavigationUuid,
+        );
+        assert.equal(navigationUuids.length, 0);
       });
     });
   });
@@ -83,104 +89,69 @@ describe("NavigationBatchPreprocessor", function() {
       firstVisitIsoDateTimeString,
     );
     /*
-  const secondVisitDateTime = parseIsoDateTimeString(
-    secondVisitIsoDateTimeString,
-  );
-  */
-
-    describe("Queue processing 5 seconds after the first visit (around the time of the second visit)", function() {
-      const nowDateTime = addSeconds(firstVisitDateTime, 5);
-      it("should not yield any navigation batches to send", async function() {
-        navigationBatchPreprocessor.navigationBatchSendQueue = [];
-        await navigationBatchPreprocessor.processQueue(nowDateTime);
-        assert.equal(
-          navigationBatchPreprocessor.navigationBatchSendQueue.length,
-          0,
-        );
-      });
-    });
+    const secondVisitDateTime = parseIsoDateTimeString(
+      secondVisitIsoDateTimeString,
+    );
+    */
 
     describe("Subsequent queue processing 12 seconds after the visit (around 7 seconds after the second visit)", function() {
       const nowDateTime = addSeconds(firstVisitDateTime, 12);
-      it("should yield relevant navigation batches to send", async function() {
-        navigationBatchPreprocessor.navigationBatchSendQueue = [];
+      it("should have found two navigation batches", async function() {
+        navigationBatchPreprocessor.navigationBatchesByNavigationUuid = {};
         await navigationBatchPreprocessor.processQueue(nowDateTime);
         assert.equal(
           navigationBatchPreprocessor.studyPayloadEnvelopeProcessQueue.length,
-          23,
+          2,
         );
-        assert.equal(
-          navigationBatchPreprocessor.navigationBatchSendQueue.length,
-          1,
+        const navigationUuids = Object.keys(
+          navigationBatchPreprocessor.navigationBatchesByNavigationUuid,
         );
-        const firstNavigationBatch: NavigationBatch =
-          navigationBatchPreprocessor.navigationBatchSendQueue[0];
-        assert.equal(firstNavigationBatch.childEnvelopes.length, 4);
-        const {
-          httpRequestCount,
-          httpResponseCount,
-          httpRedirectCount,
-          javascriptOperationCount,
-        } = firstNavigationBatch;
+        assert.equal(navigationUuids.length, 2);
+        const oneNavigationBatch: NavigationBatch =
+          navigationBatchPreprocessor.navigationBatchesByNavigationUuid[
+            "290cb5b2-828c-4eec-9626-69463b7b4d05"
+          ];
+        assert.equal(oneNavigationBatch.childEnvelopes.length, 4);
         assert.deepStrictEqual(
           [
-            httpRequestCount,
-            httpResponseCount,
-            httpRedirectCount,
-            javascriptOperationCount,
+            oneNavigationBatch.httpRequestCount,
+            oneNavigationBatch.httpResponseCount,
+            oneNavigationBatch.httpRedirectCount,
+            oneNavigationBatch.javascriptOperationCount,
           ],
           [2, 2, 0, 0],
         );
-      });
-    });
 
-    describe("Subsequent queue processing 17 seconds after the visit (around 12 seconds after the second visit)", function() {
-      const nowDateTime = addSeconds(firstVisitDateTime, 17);
-      it("should yield relevant navigation batches to send", async function() {
-        navigationBatchPreprocessor.navigationBatchSendQueue = [];
-        await navigationBatchPreprocessor.processQueue(nowDateTime);
-        assert.equal(
-          navigationBatchPreprocessor.studyPayloadEnvelopeProcessQueue.length,
-          0,
-        );
-        assert.equal(
-          navigationBatchPreprocessor.navigationBatchSendQueue.length,
-          1,
-        );
-        const firstNavigationBatch: NavigationBatch =
-          navigationBatchPreprocessor.navigationBatchSendQueue[0];
-        assert.equal(firstNavigationBatch.childEnvelopes.length, 22);
-        const {
-          httpRequestCount,
-          httpResponseCount,
-          httpRedirectCount,
-          javascriptOperationCount,
-        } = firstNavigationBatch;
+        const anotherNavigationBatch: NavigationBatch =
+          navigationBatchPreprocessor.navigationBatchesByNavigationUuid[
+            "bd1e0e8e-6c72-4a93-983b-d59c336b4472"
+          ];
+        assert.equal(anotherNavigationBatch.childEnvelopes.length, 22);
         assert.deepStrictEqual(
           [
-            httpRequestCount,
-            httpResponseCount,
-            httpRedirectCount,
-            javascriptOperationCount,
+            anotherNavigationBatch.httpRequestCount,
+            anotherNavigationBatch.httpResponseCount,
+            anotherNavigationBatch.httpRedirectCount,
+            anotherNavigationBatch.javascriptOperationCount,
           ],
           [11, 10, 1, 0],
         );
       });
     });
 
-    describe("Subsequent queue processing 25 seconds after the visit (around 20 seconds after the second visit)", function() {
-      const nowDateTime = addSeconds(firstVisitDateTime, 25);
-      it("should not yield any navigation batches to send", async function() {
-        navigationBatchPreprocessor.navigationBatchSendQueue = [];
+    describe("Subsequent queue processing 10 hours after after the visits", function() {
+      const nowDateTime = addSeconds(firstVisitDateTime, 10 * 60 * 60);
+      it("should have purged all navigation batches", async function() {
+        navigationBatchPreprocessor.navigationBatchesByNavigationUuid = {};
         await navigationBatchPreprocessor.processQueue(nowDateTime);
         assert.equal(
           navigationBatchPreprocessor.studyPayloadEnvelopeProcessQueue.length,
           0,
         );
-        assert.equal(
-          navigationBatchPreprocessor.navigationBatchSendQueue.length,
-          0,
+        const navigationUuids = Object.keys(
+          navigationBatchPreprocessor.navigationBatchesByNavigationUuid,
         );
+        assert.equal(navigationUuids.length, 0);
       });
     });
   });
@@ -200,33 +171,23 @@ describe("NavigationBatchPreprocessor", function() {
       firstVisitIsoDateTimeString,
     );
 
-    describe("Queue processing 5 seconds after the visit", function() {
-      const nowDateTime = addSeconds(firstVisitDateTime, 5);
-      it("should not yield any navigation batches to send", async function() {
-        navigationBatchPreprocessor.navigationBatchSendQueue = [];
-        await navigationBatchPreprocessor.processQueue(nowDateTime);
-        assert.equal(
-          navigationBatchPreprocessor.navigationBatchSendQueue.length,
-          0,
-        );
-      });
-    });
-
     describe("Queue processing 20 seconds after the visit", function() {
       const nowDateTime = addSeconds(firstVisitDateTime, 20);
-      it("should yield relevant navigation batches to send", async function() {
-        navigationBatchPreprocessor.navigationBatchSendQueue = [];
+      it("should have found two navigation batches", async function() {
+        navigationBatchPreprocessor.navigationBatchesByNavigationUuid = {};
         await navigationBatchPreprocessor.processQueue(nowDateTime);
         assert.equal(
           navigationBatchPreprocessor.studyPayloadEnvelopeProcessQueue.length,
-          0,
-        );
-        assert.equal(
-          navigationBatchPreprocessor.navigationBatchSendQueue.length,
           1,
         );
+        const navigationUuids = Object.keys(
+          navigationBatchPreprocessor.navigationBatchesByNavigationUuid,
+        );
+        assert.equal(navigationUuids.length, 1);
         const firstNavigationBatch: NavigationBatch =
-          navigationBatchPreprocessor.navigationBatchSendQueue[0];
+          navigationBatchPreprocessor.navigationBatchesByNavigationUuid[
+            navigationUuids[0]
+          ];
         assert.equal(firstNavigationBatch.childEnvelopes.length, 15);
         const {
           httpRequestCount,
@@ -243,6 +204,22 @@ describe("NavigationBatchPreprocessor", function() {
           ],
           [2, 2, 0, 11],
         );
+      });
+    });
+
+    describe("Queue processing 10 hours after the visit", function() {
+      const nowDateTime = addSeconds(firstVisitDateTime, 10 * 60 * 60);
+      it("should have purged all navigation batches", async function() {
+        navigationBatchPreprocessor.navigationBatchesByNavigationUuid = {};
+        await navigationBatchPreprocessor.processQueue(nowDateTime);
+        assert.equal(
+          navigationBatchPreprocessor.studyPayloadEnvelopeProcessQueue.length,
+          0,
+        );
+        const navigationUuids = Object.keys(
+          navigationBatchPreprocessor.navigationBatchesByNavigationUuid,
+        );
+        assert.equal(navigationUuids.length, 0);
       });
     });
   });
@@ -270,33 +247,23 @@ describe("NavigationBatchPreprocessor", function() {
       firstVisitIsoDateTimeString,
     );
 
-    describe("Queue processing 5 seconds after the visit", function() {
-      const nowDateTime = addSeconds(firstVisitDateTime, 5);
-      it("should not yield any navigation batches to send", async function() {
-        navigationBatchPreprocessor.navigationBatchSendQueue = [];
-        await navigationBatchPreprocessor.processQueue(nowDateTime);
-        assert.equal(
-          navigationBatchPreprocessor.navigationBatchSendQueue.length,
-          0,
-        );
-      });
-    });
-
     describe("Queue processing 20 seconds after the visit", function() {
       const nowDateTime = addSeconds(firstVisitDateTime, 20);
-      it("should yield relevant navigation batches to send", async function() {
-        navigationBatchPreprocessor.navigationBatchSendQueue = [];
+      it("should have found two navigation batches", async function() {
+        navigationBatchPreprocessor.navigationBatchesByNavigationUuid = {};
         await navigationBatchPreprocessor.processQueue(nowDateTime);
         assert.equal(
           navigationBatchPreprocessor.studyPayloadEnvelopeProcessQueue.length,
-          0,
-        );
-        assert.equal(
-          navigationBatchPreprocessor.navigationBatchSendQueue.length,
           1,
         );
+        const navigationUuids = Object.keys(
+          navigationBatchPreprocessor.navigationBatchesByNavigationUuid,
+        );
+        assert.equal(navigationUuids.length, 1);
         const firstNavigationBatch: NavigationBatch =
-          navigationBatchPreprocessor.navigationBatchSendQueue[0];
+          navigationBatchPreprocessor.navigationBatchesByNavigationUuid[
+            navigationUuids[0]
+          ];
         assert.equal(firstNavigationBatch.childEnvelopes.length, 15);
         const {
           httpRequestCount,
@@ -313,12 +280,22 @@ describe("NavigationBatchPreprocessor", function() {
           ],
           [2, 2, 0, 11],
         );
+      });
+    });
 
-        /*
-        await this.telemetrySender.sendStudyPayloadEnvelope(
-          studyPayloadEnvelope,
+    describe("Queue processing 10 hours after the visit", function() {
+      const nowDateTime = addSeconds(firstVisitDateTime, 10 * 60 * 60);
+      it("should have purged all navigation batches", async function() {
+        navigationBatchPreprocessor.navigationBatchesByNavigationUuid = {};
+        await navigationBatchPreprocessor.processQueue(nowDateTime);
+        assert.equal(
+          navigationBatchPreprocessor.studyPayloadEnvelopeProcessQueue.length,
+          0,
         );
-        */
+        const navigationUuids = Object.keys(
+          navigationBatchPreprocessor.navigationBatchesByNavigationUuid,
+        );
+        assert.equal(navigationUuids.length, 0);
       });
     });
   });
