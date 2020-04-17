de8383188396d8514fcc8fa9253e077e8b5d891c
Motin <motin@motin.eu>
2020-04-17 14:35:53 +0300

    Improved error display

--- extension/ts/shared-react-resources/DisplayError.tsx
+++ extension/ts/shared-react-resources/DisplayError.tsx
@@ -1,20 +1,40 @@
 import * as React from "react";
+import { MouseEvent } from "react";
 
 export class DisplayError extends React.Component<{}, {}> {
   constructor(props) {
     super(props);
   }
 
+  cancel(event: MouseEvent) {
+    event.preventDefault();
+    window.close();
+  }
+
   render() {
     return (
-      <header className="panel-section panel-section-header">
-        <div className="icon-section-header">
-          <img src="../icons/green-extensionsicon.svg" width="32" height="32" />
+      <>
+        <header className="panel-section panel-section-header">
+          <div className="icon-section-header">
+            <img
+              src="../icons/green-extensionsicon.svg"
+              width="32"
+              height="32"
+            />
+          </div>
+          <div className="text-section-header">
+            Could not display the "YouTube Regret" form
+          </div>
+        </header>
+        <div className="panel-section panel-section-formElements">
+          <span className="text-center">Try reloading the page and try again</span>
         </div>
-        <div className="text-section-header">
-          This extension encountered an error
-        </div>
-      </header>
+        <footer className="panel-section panel-section-footer">
+          <div onClick={this.cancel} className="panel-section-footer-button">
+            Close
+          </div>
+        </footer>
+      </>
     );
   }
 }
