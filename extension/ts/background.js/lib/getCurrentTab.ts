3b424d70e1adf7d4f6068ccc2da4507d516d1672
Motin <motin@motin.eu>
2020-04-27 16:06:36 +0300

    Current tab correctly filters on current window only

--- extension/ts/background.js/lib/getCurrentTab.ts
+++ extension/ts/background.js/lib/getCurrentTab.ts
@@ -4,6 +4,7 @@ import Tab = Tabs.Tab;
 export const getCurrentTab = async (): Promise<Tab | null> => {
   const activeTabs = await browser.tabs.query({
     active: true,
+    currentWindow: true,
   });
   if (activeTabs.length > 0) {
     return activeTabs[0];
