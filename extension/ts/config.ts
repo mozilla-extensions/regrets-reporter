633b261edeb49eb1fb726736145c8e7e57b7ec5d
Motin <motin@motin.eu>
2020-05-26 16:07:05 +0300

    Set link to product feedback survey

--- extension/ts/config.ts
+++ extension/ts/config.ts
@@ -2,5 +2,6 @@ export const config = {
   sentryDsn: process.env.SENTRY_DSN,
   telemetryServer: process.env.TELEMETRY_SERVER,
   privacyNoticeUrl: "about:blank",
-  feedbackSurveyUrl: "about:blank",
+  feedbackSurveyUrl:
+    "https://qsurvey.mozilla.com/s3/Regrets-Reporter-Product-Feedback",
 };
