ab0c6ed808fc295de9900442ec7fe76ddf991c50
Motin <motin@motin.eu>
2020-07-07 21:17:29 +0300

    Include more error context if video_description fails to extract

--- extension/ts/background.js/ReportSummarizer.ts
+++ extension/ts/background.js/ReportSummarizer.ts
@@ -612,18 +612,25 @@ export class ReportSummarizer {
     }
 
     let video_description;
+    let errorContext;
     try {
       const twoColumnWatchNextResultsResultsResultsContentsWithVideoSecondaryInfoRenderer = ytInitialData.contents.twoColumnWatchNextResults.results.results.contents.find(
         contents => {
           return contents.videoSecondaryInfoRenderer !== undefined;
         },
       );
+      errorContext = {
+        twoColumnWatchNextResultsResultsResultsContentsWithVideoSecondaryInfoRenderer,
+      };
       video_description = runsToMarkdown(
         twoColumnWatchNextResultsResultsResultsContentsWithVideoSecondaryInfoRenderer
           .videoSecondaryInfoRenderer.description.runs,
       );
     } catch (err) {
-      captureExceptionWithExtras(err, { attribute: "video_description" });
+      captureExceptionWithExtras(err, {
+        attribute: "video_description",
+        errorContext,
+      });
       console.error("video_description", err.message);
       video_description = "<failed>";
     }
