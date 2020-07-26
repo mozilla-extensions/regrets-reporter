b989d9bc4a46014188fb73a621b407f50eeacb72
Motin <motin@motin.eu>
2020-07-26 23:02:27 +0300

    Fix build id extraction on chromium browsers

--- extension/ts/background.js/lib/getChromiumBrowserInfo.ts
+++ extension/ts/background.js/lib/getChromiumBrowserInfo.ts
@@ -1,9 +1,19 @@
 export const getChromiumBrowserInfo = () => {
-  const chromeVersion = /Chrome\/([0-9.]+)/.exec(navigator.userAgent)[1];
+  const chromeVersionAndBuildId = /Chrome\/([0-9.]+)/.exec(
+    navigator.userAgent,
+  )[1];
+  const buildID = chromeVersionAndBuildId
+    .split(".")
+    .slice(2, 4)
+    .join(".");
+  const version = chromeVersionAndBuildId
+    .split(".")
+    .slice(0, 2)
+    .join(".");
   return {
-    buildID: "",
+    buildID,
     vendor: "Google",
-    version: chromeVersion,
+    version,
     name: "Chrome",
   };
 };
