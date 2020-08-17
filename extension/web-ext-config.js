d164fe4ffd3965c93f82773b143e05430eb3e35c
Motin <motin@motin.eu>
2020-08-17 14:14:56 +0300

    Formatting

--- extension/web-ext-config.js
+++ extension/web-ext-config.js
@@ -26,7 +26,7 @@ const defaultConfig = {
     ],
     pref: ["browser.ctrlTab.recentlyUsedOrder=false"],
   },
-  filename: `{name}-{version}-${targetBrowser}.xpi`
+  filename: `{name}-{version}-${targetBrowser}.xpi`,
 };
 
 if (targetBrowser === "firefox") {
