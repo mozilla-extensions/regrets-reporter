d1535909c794a68ea55eb90d7c500b314077d4ec
Jesse McCrosky <mccrosky@gmail.com>
2022-05-16 15:08:20 +0300

    Fix OOO issue

--- analysis/semsim/utils.py
+++ analysis/semsim/utils.py
@@ -4,7 +4,7 @@ import gcld3
 def filter_lang(data, with_transcript, lang):
     detector = gcld3.NNetLanguageIdentifier(min_num_bytes=0, max_num_bytes=1000)
     processed_data = data.copy(deep=True)
-    for part in ['regret_title', 'recommendation_title', 'regret_description', 'recommendation_description'] + ['regret_transcript', 'recommendation_transcript'] if with_transcript else []:
+    for part in ['regret_title', 'recommendation_title', 'regret_description', 'recommendation_description'] + (['regret_transcript', 'recommendation_transcript'] if with_transcript else []):
         processed_data = processed_data[
             (processed_data[part].str.len() < 10) |
             [detector.FindLanguage(text=x).language == lang for x in processed_data[part]]
